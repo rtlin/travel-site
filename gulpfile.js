851952627ddbeb82d272060d8a5dbc2613c3511e
unknown <rtlin99@gmail.com>
2017-10-29 21:25:49 -0400

    add all

---
+++ gulpfile.js
@@ -0,0 +1,30 @@
+var gulp = require('gulp');
+var watch = require('gulp-watch');
+var postcss = require('gulp-postcss');
+var autoprefixer = require('autoprefixer');
+var cssvars = require('postcss-simple-vars');
+var nested = require('postcss-nested');
+
+gulp.task('default', function(){
+	console.log("Hooray - you created a Gulp tsk");
+});
+
+gulp.task('html', function(){
+ console.log("html here.");
+});
+gulp.task('styles', function(){
+	console.log("style here");
+	return gulp.src('./app/assets/styles/styles.css')
+		.pipe(postcss([cssvars, nested, autoprefixer]))
+		.pipe(gulp.dest('./app/temp/styles'));
+});
+
+gulp.task('watch', function(){
+	watch('./app/index.html', function(){
+		gulp.start('html');
+	});
+	watch('./app/assets/styles/**/*.css', function(){
+		gulp.start("styles");
+	});
+});
+
