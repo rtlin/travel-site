738f59cb1158f1022f129277cac990e9c8d970bc
Robert Lin <rtlin99@gmail.com>
2017-11-10 11:42:50 -0500

    Completed the reveal on scroll behavior

---
+++ app/assets/scripts/modules/RevealOnScroll.js
@@ -0,0 +1,40 @@
+import $ from 'jquery';
+import waypoints from '../../../../node_modules/waypoints/lib/noframework.waypoints';
+
+class RevealOnScroll{
+	/*
+	constructor(){		
+		this.itemsToReveal = $(".feature-item .testimonial");
+		this.hideInitially();
+		this.createWayPoints();
+	}
+	*/
+	constructor(els, offset){		
+		this.itemsToReveal = els;
+		this.offsetPercentage = offset;
+		this.hideInitially();		
+		this.createWayPoints();		
+	}
+	
+	hideInitially(){
+		this.itemsToReveal.addClass("reveal-item");
+	}
+	
+	createWayPoints(){
+		var that = this;
+		this.itemsToReveal.each(function(){
+			var currentItem = this;
+			new Waypoint({
+				element:currentItem,
+				handler: function(){
+					$(currentItem).addClass("reveal-item--is-visible");
+				},
+				//offset: "85%"
+				offset: that.offsetPercentage
+			});
+		});
+	}
+	
+}
+
+export default RevealOnScroll;
